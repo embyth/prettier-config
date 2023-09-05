74fb6659b7b1c4aea6b5aa8fef68072a6c7ce367
Rostyslav Miniukov <miniukovrostyslav@gmail.com>
2023-09-05 21:27:32 +0300

    :tada: Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'prettier';
+
+const config: Config = {
+  printWidth: 120, // max 120 chars in line, code is easy to read
+  useTabs: false, // use spaces instead of tabs
+  tabWidth: 2, // "visual width" of of the "tab"
+  trailingComma: 'all', // add trailing commas in objects, arrays, etc.
+  semi: true, // add ; when needed
+  singleQuote: true, // '' for stings instead of ""
+  bracketSpacing: true, // import { some } ... instead of import {some} ...
+  arrowParens: 'always', // braces even for single param in arrow functions (a) => { }
+  jsxSingleQuote: false, // "" for react props, like in html
+  bracketSameLine: false, // pretty JSX
+  endOfLine: 'lf', // 'lf' for linux, 'crlf' for windows, we need to use 'lf' for git
+  proseWrap: 'always', // printWidth line breaks in md/mdx files
+};
+
+export default config;
---
+++ test/__test__.ts
@@ -0,0 +1,62 @@
+// const obj = {
+//   one: 1,
+
+//   two: 2,
+//    three: 3
+// }
+
+// ;("this config avoids semicolons at the end of statements and uses single quotes for strings")
+
+// const arrowFunctions: (
+//     arg:  boolean) => number = avoidParens => 1
+
+// interface MyInterface {
+//   foo():  string,
+//   bar: Array<number>
+// }
+
+// export abstract class Foo implements MyInterface {
+//   foo() {
+//             // TODO: return an actual value here
+//         return 'hello'
+//       }
+//   get bar() {
+//     return [  1,
+
+//       2, 3,
+//     ]
+//   }
+// }
+
+// type RequestType = 'GET' | 'HEAD' | 'POST' | 'PUT' | 'OPTIONS' | 'CONNECT' | 'DELETE' | 'TRACE'
+
+const obj = {
+  one: 1,
+  two: 2,
+  three: 3,
+};
+
+('this config avoids semicolons at the end of statements and uses single quotes for strings');
+
+const arrowFunctions: (arg: boolean) => number = (avoidParens) => 1;
+
+interface MyInterface {
+  foo(): string;
+  bar: Array<number>;
+}
+
+export abstract class Foo implements MyInterface {
+  foo() {
+    // TODO: return an actual value here
+    return 'hello';
+  }
+  get bar() {
+    return [
+      1,
+
+      2, 3,
+    ];
+  }
+}
+
+type RequestType = 'GET' | 'HEAD' | 'POST' | 'PUT' | 'OPTIONS' | 'CONNECT' | 'DELETE' | 'TRACE';
